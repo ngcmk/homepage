a4e3b42c1b735d4ee7c63b570869a5bb068ab570
echo <petrovskidimitar@yandex.com>
2026-04-10 23:56:53 +0200

    fix: configure cdn.sanity.io in next.config.js

--- next.config.js
+++ next.config.js
@@ -2,6 +2,14 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cdn.sanity.io',
+      },
+    ],
+  },
   async redirects() {
     return [
       {
